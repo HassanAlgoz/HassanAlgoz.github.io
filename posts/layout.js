9df339e2dca2e587182558188dff239b09f40406
Hassan Algoz <HassanAlgoz@gmail.com>
2020-12-11 20:48:47 +0300

    update

--- posts/layout.js
+++ posts/layout.js
@@ -1,10 +1,21 @@
 const READING_SPEED = 200; // words per minute
 
 window.addEventListener("load", () => {
-    const $body = document.getElementsByClassName("post-body").item(0);
-    const words = $body.textContent.split(" ");
-    const time = words.length / READING_SPEED;
 
-    const $readingTime = document.getElementsByClassName("reading-time").item(0);
-    $readingTime.textContent = `${Math.ceil(time).toFixed(0)} د`;
+    (function calculateReadingTime() {
+        const $body = document.getElementsByClassName("post-body").item(0);
+        const words = $body.textContent.split(" ").filter(w => w.length >= 3);
+        const time = words.length / READING_SPEED;
+
+        const $readingTime = document.getElementsByClassName("reading-time").item(0);
+        $readingTime.textContent = `${Math.ceil(time).toFixed(0)} د`;
+    })();
+
+    (function HighlightCodeBlocks() {
+        document.querySelectorAll("pre code").forEach((block) => {
+            console.log("hljs:", hljs);
+            hljs.highlightBlock(block);
+        });
+    })();
 });
+
